b7a7e4e24adf3b3d817584e59fdd7b311951a8dd
shibaayan <shibaayanm@gmail.com>
2019-07-24 06:57:19 +0530

    adding libs.py

---
+++ libs.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.call('pip install tweepy', shell = True)
+subprocess.call('pip install sklearn', shell = True)
+subprocess.call('pip install nltk', shell = True)
+subprocess.call('pip install matplotlib', shell = True)
+subprocess.call('pip install tqdm', shell = True)
